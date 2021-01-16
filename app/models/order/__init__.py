4a6068330d63b09dad954f2cebf3927022ef097d
tycarry <2501160661@qq.com>
2021-01-16 17:03:07 +0800

    modify

--- app/models/order/__init__.py
+++ app/models/order/__init__.py
@@ -4,3 +4,15 @@
 # @IDE: PyCharm
 # @Create time: 1/11/21 10:25 PM
 # @Description: 订单
+from app.models.order import entry, express, logistic, order, order_status, partner, snapshot
+
+
+def all():
+    result = []
+    models = [entry, express, logistic, order, order_status, partner, snapshot]
+    for model in models:
+        result += model.__all__
+    return result
+
+
+__all__ = all()
