a479512b16ba8696fe890a1c66fafb135b7db9ca
tycarry <2501160661@qq.com>
2021-01-16 22:13:20 +0800

    modify

--- app/models/content/__init__.py
+++ app/models/content/__init__.py
@@ -4,3 +4,16 @@
 # @IDE: PyCharm
 # @Create time: 1/11/21 10:25 PM
 # @Description:
+from app.models.content import banner, board, post
+
+
+def all():
+    result = []
+    models = [banner, board, post]
+    for model in models:
+        result += model.__all__
+
+    return result
+
+
+__all__ = all()
--- app/models/coupon/__init__.py
+++ app/models/coupon/__init__.py
@@ -4,3 +4,16 @@
 # @IDE: PyCharm
 # @Create time: 1/11/21 10:26 PM
 # @Description: 优惠卷
+from app.models.coupon import coupon, wallet
+
+
+def all():
+    result = []
+    models = [coupon, wallet]
+    for model in models:
+        result += model.__all__
+
+    return result
+
+
+__all__ = all()
